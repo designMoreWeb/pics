8009fef1213cf19a98b8053b5815a97917e9b2a7
designMoreWeb <divystolia@gmail.com>
2019-01-14 10:59:58 -0500

    some basic css

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import Unsplash from'../api/Unsplash';
 import ImageList from './ImageList';
+import './App.css';
 
 class App extends React.Component{
     state = {images: []};
@@ -20,6 +21,8 @@ class App extends React.Component{
     render(){
     return (
     <div className="ui container" style={{marginTop: '10px'}}>
+        <h2 className="page-title">Image Finder</h2>
+        <p className="page-instructions">Find images from unsplash.io by searching in the search bar</p>
         <SearchBar onSearch={this.onSearchSubmit} />
         <ImageList images={this.state.images}/>
     </div>
