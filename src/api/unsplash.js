0eb7f675e5154b0e8a2c53f56dbb6d48122efed4
designMoreWeb <divystolia@gmail.com>
2019-01-14 10:11:55 -0500

    fixed url

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 
 export default Axios.create({
-    baseURL: 'https: //api.unpslash.com',
+    baseURL: 'https://api.unpslash.com',
     headers: {
         Authorization:'Client-ID 99d043cafac1caa8807a6312ba12ca7a70846b2c27e463f16360270565786993'
     }
