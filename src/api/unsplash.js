7b7ac60617eafbedc6da770a86ea60a7128dbf8b
designMoreWeb <divystolia@gmail.com>
2019-01-14 10:28:57 -0500

    fixed network promise error

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,9 @@
 import Axios from 'axios';
 
-
 export default Axios.create({
-    baseURL: 'https://api.unpslash.com',
+    baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization:'Client-ID 99d043cafac1caa8807a6312ba12ca7a70846b2c27e463f16360270565786993'
+        Authorization: 'Client-ID 99d043cafac1caa8807a6312ba12ca7a70846b2c27e463f16360270565786993'
+
     }
 });
